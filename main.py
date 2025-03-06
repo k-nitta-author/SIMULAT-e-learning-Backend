b0e6f7dc0a1cad405ffc409263e039713a03132b
k-nitta-author <k.nitta.author@gmail.com>
2025-03-06 16:40:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import flask
 from flask import Flask, request, jsonify, make_response
 
 # the below are for configuring the token
+from flask import app
 import jwt
 from functools import wraps
 from datetime import datetime, timedelta
@@ -23,14 +24,24 @@ from resources.badge import BadgeResource
 from resources.term import TermResource
 
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
 
 from setup import APP
 
 # enable cors
 cors = CORS(APP, resources={r"/*": {"origins": "*"}})
 
-APP.config['SECRET KEY'] = environ.get("SECRET_KEY")
+APP.config['SECRET_KEY'] = environ.get("SECRET_KEY")
+
+# Set up the database
+APP.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URL")
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+APP.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_timeout': 30,  # Set the pool timeout to 30 seconds
+    'pool_recycle': 1800  # Recycle connections every 30 minutes
+}
+
+db = SQLAlchemy(APP)
 
 user_resource = UserResource()
 course_resource = CourseResource()
@@ -49,9 +60,10 @@ assignment_score_res = AssignmentScoreResource()
 badge_res = BadgeResource()
 term_res = TermResource()
 
-
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db.session.remove()
 
 if __name__ == '__main__':
-    
     APP.run(debug=True, host='0.0.0.0', port=10000)
 
