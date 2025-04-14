4b1188a5616447eeb35f4f12004767006ba5f5f3
k-nitta-author <k.nitta.author@gmail.com>
2025-04-14 14:16:10 +0800

    Update user.py

--- resources/user.py
+++ resources/user.py
@@ -548,7 +548,16 @@ class UserResource():
                 'exp': datetime.now() + timedelta(seconds=10),
                 'roles': table.get_roles_list(u)}, APP.secret_key)
 
-            return jsonify({"token": token, "user_id": u.id}), 200
+            return jsonify({
+                "token": token, 
+                "user_id": u.id,
+                "privileges": {
+                    "is_admin": u.is_admin,
+                    "is_student": u.is_student,
+                    "is_instructor": u.is_instructor,
+                    "is_super_admin": u.is_super_admin
+                }
+            }), 200
 
         return jsonify({"message": "Invalid credentials"}), 401
 
